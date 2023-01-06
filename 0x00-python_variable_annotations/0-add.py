6a43dc4a81e1184acd2f56f2fb40106d44b711ce
michael03nyamwaya <99642769+michael03nyamwaya@users.noreply.github.com>
2023-01-06 13:14:21 +0300

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' type-annotated function add'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Args:
+        a: float
+        b: float
+    Return:
+        sum(a,b) as a float.
+    '''
+    return a + b
