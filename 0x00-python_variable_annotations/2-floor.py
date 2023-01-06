22e30eee56cec3610450a692cc53d192861e2e11
michael03nyamwaya <99642769+michael03nyamwaya@users.noreply.github.com>
2023-01-06 13:15:59 +0300

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function floor'''
+
+
+def floor(n: float) -> int:
+    '''
+    Args:
+        n: float
+    Return:
+        floor(n) as int.
+    '''
+    return int(n)
