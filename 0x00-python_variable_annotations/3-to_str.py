dcdec72e95325ba52881a87c0ea5b8e463ddb8b1
michael03nyamwaya <99642769+michael03nyamwaya@users.noreply.github.com>
2023-01-06 13:17:12 +0300

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function to_str'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Args:
+        n: float
+    Return:
+        str rep of n.
+    '''
+    return str(n)
